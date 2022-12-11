777aa65dc17166e911a5ac857f91669a1ba6b2eb
neeraj <neerajgrg93@gmail.com>
2022-12-11 10:27:17 +0530

    updated files

--- blocks/hero/doc-table.js
+++ blocks/hero/doc-table.js
@@ -1,6 +1,6 @@
 [
   ["  HERO "],
   [
-    "https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/hero/preview.jpeg"
+    "https://raw.githubusercontent.com/neerajgrg/HelloFranklin/main/blocks/hero/preview.jpeg"
   ],
 ];
