51bb8dd3312439d523ec296cd105e60de8f77253
neeraj <neerajgrg93@gmail.com>
2022-12-11 10:11:54 +0530

    updated files

---
+++ blocks/header/doc-table.js
@@ -0,0 +1,5 @@
+[
+     ['  HEADER '],
+    ['Row 2, Cell 1', 'Row 2, Cell 2']
+];
+
---
+++ blocks/hero/doc-table.js
@@ -0,0 +1,6 @@
+[
+  ["  HERO "],
+  [
+    "https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/hero/preview.jpeg"
+  ],
+];
