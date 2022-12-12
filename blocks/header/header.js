325a83ccdb4bd7df6b6cd4650a5fa590664efcf3
neeraj <neerajgrg93@gmail.com>
2022-12-12 10:19:57 +0530

    updated

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -10,8 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import {  readBlockConfig } from '../../scripts/scripts.js';
-import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons, readBlockConfig } from '../../scripts/lib-franklin.js';
 
 /**
  * decorates the header, mainly the nav
